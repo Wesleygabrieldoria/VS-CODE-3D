a546dcec0b687a781214a7f8b7b88951dbe42fce
WesleyDoria <110678889+Wesleygabrieldoria@users.noreply.github.com>
2024-05-09 07:42:43 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
-const botoes = document.querySelector(".botao");
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-05-10T00:00:00");
 
-for (let i=0;i<botoes.lenght;i++){
-    botoes[i].onclik=function(){
-        for(j=0;j<botoes.leght;j++){
+contadores[0].textContent = tempoObjetivo1;
+
+
+for (let i=0; i<botoes.length;i++){
+    botoes[i].onclick=function(){
+        for(j=0;j<botoes.length;j++){
             botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
         }
         botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
     }
 }
